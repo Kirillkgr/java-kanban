fd5030bad5148beae841f2ce80b19cb81989e457
Kirillkgr <RillGd@gmail.com>
2024-09-09 20:52:10 +0200

    Убрал класс LinkedListOfHistory по рекомендации также проставил приватный модификатор доступа. Переименовал некоторые методы для большей информативности.

--- src/Manager/Impl/InMemoryHistoryManager.java
+++ src/Manager/Impl/InMemoryHistoryManager.java
@@ -1,89 +1,78 @@
 package Manager.Impl;
 
-import Manager.HistoryManager;
-import Manager.Node;
 import Models.Task;
+import Manager.Node;
+import Manager.HistoryManager;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class InMemoryHistoryManager implements HistoryManager {
-
-    private final LinkedListOfHistory<Task> historyTask;
+    final HashMap<Integer, Node<Task>> mapOfHistory;
+    private Node<Task> head;// Родительский объект
+    private Node<Task> tail;// Дочерний объект
 
     public InMemoryHistoryManager() {
-        historyTask = new LinkedListOfHistory<>();
+        this.mapOfHistory = new HashMap<>();
     }
 
     @Override
     public void add(Task task) {
-        historyTask.linkLastObject(task);
+        linkLastObject(task);
     }
 
     @Override
     public void remove(int id) {
-        historyTask.remove(id);
+        removeHistoryById(id);
     }
 
     @Override
     public LinkedList<Task> getHistory() {
-        return historyTask.getTasks();
+        return getAllTasksFromHistory();
     }
 
-    static final class LinkedListOfHistory<T extends Task> {
-        final HashMap<Integer, Node<T>> mapOfHistory;
-        private Node<T> head;// Родительский объект
-        private Node<T> tail;// Дочерний объект
-
-        public LinkedListOfHistory() {
-            mapOfHistory = new HashMap<>();
-            head = null;
-            tail = null;
+    private void linkLastObject(Task task) {
+        if (mapOfHistory.containsKey(task.getId())) {
+            remove(task.getId());
+        } // Если существует удаляем
+        Node<Task> newNode = new Node<>(tail, null, task);
+        if (tail != null) {// Если существует дочерний, то указываем что следующем будит новый объект
+            tail.setNext(newNode);
         }
-
-        public void linkLastObject(T task) {
-            if (mapOfHistory.containsKey(task.getId())) {
-                remove(task.getId());
-            } // Если существует удаляем
-            Node<T> newNode = new Node<>(tail, null, task);
-            if (tail != null) {// Если существует дочерний, то указываем что следующем будит новый объект
-                tail.setNext(newNode);
-            }
-                tail = newNode;// Иначе новый будит дочерним
-            if (head == null) {
-                head = tail;
-            }
-            mapOfHistory.put(task.getId(), newNode); // помещаем новый в мапу
+        tail = newNode;// Иначе новый будит дочерним
+        if (head == null) {
+            head = tail;
         }
+        mapOfHistory.put(task.getId(), newNode); // помещаем новый в мапу
+    }
 
-        public void remove(int id) {
-            Node<T> nodeToRemove = mapOfHistory.remove(id);
-            if (nodeToRemove != null) {
-                removeNode(nodeToRemove);
-            }
+    private void removeHistoryById(int id) {
+        Node<Task> nodeToRemove = mapOfHistory.remove(id);
+        if (nodeToRemove != null) {
+            removeNode(nodeToRemove);
         }
+    }
 
-        private void removeNode(Node<T> node) {
-            if (node.getPrev() != null) {
-                node.getPrev().setNext(node.getNext());
-            } else {
-                head = node.getNext();
-            }
-            if (node.getNext() != null) {
-                node.getNext().setPrev(node.getPrev());
-            } else {
-                tail = node.getPrev();
-            }
+    private void removeNode(Node<Task> node) {
+        if (node.getPrev() != null) {
+            node.getPrev().setNext(node.getNext());
+        } else {
+            head = node.getNext();
         }
+        if (node.getNext() != null) {
+            node.getNext().setPrev(node.getPrev());
+        } else {
+            tail = node.getPrev();
+        }
+    }
 
-        public LinkedList<Task> getTasks() {
-            LinkedList<Task> tasks = new LinkedList<>();
-            Node<T> current = head;
-            while (current != null) {
-                tasks.add(current.getElement());
-                current = current.getNext();
-            }
-            return tasks;
+    private LinkedList<Models.Task> getAllTasksFromHistory() {
+        LinkedList<Models.Task> tasks = new LinkedList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            tasks.add(current.getElement());
+            current = current.getNext();
         }
+        return tasks;
     }
 }
